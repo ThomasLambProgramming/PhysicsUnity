a85046a364a1327472b976db2402a3d814732378
Thomas Lamb <mcsorleyprogramming@gmail.com>
2021-03-01 15:44:21 +1100

    Added movement and camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float mouseSens = 100.0f;
+    public Transform player;
+    private float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpForce = 10.0f;
+    
+    //groundchecks
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    //this is so it will only detect what we want (not detecting player for ground check)
+    public LayerMask groundMask;
+       
+    
+    private Vector3 velocity;
+    private bool isGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            //this sets default to slightly press down on the player 
+            velocity.y = -1f;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        //the right transform is a directional vector -1 - 1, same for forward 
+        //so its just making a vector based on the amounts of the axis input then 
+        //position + move * speed thing
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * (speed * Time.deltaTime));
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = jumpForce * Time.deltaTime;
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
